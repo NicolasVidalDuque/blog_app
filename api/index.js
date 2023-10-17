f833ba1d31d8d944a83998efdba7c9ca2e323b78
Nicolas Vidal <vidalnicolasd@gmail.com>
2023-10-17 08:20:00 -0500

    no cors2

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const Post = require('./models/Post');
