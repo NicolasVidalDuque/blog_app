a10ba891416802cc42fc8e73e9c67d89bfe34b88
Nicolas Vidal <vidalnicolasd@gmail.com>
2023-10-16 20:41:06 -0500

    ccc

--- client/src/host.js
+++ client/src/host.js
@@ -1,2 +1,2 @@
-export const HOST = "http://localhost:4000"
-// "http://myblog-nvd.onrender.com";
+// export const HOST = "http://localhost:4000"
+export const HOST = "http://myblog-nvd.onrender.com";
