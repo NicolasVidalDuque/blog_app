e1565ffa2763d23edd221d94db97d3ef42b33584
NicolasVidalDuque <vidalnicolasd@gmail.com>
2023-11-09 16:48:23 -0500

    changed HOST link

--- client/src/host.js
+++ client/src/host.js
@@ -1,2 +1,2 @@
-// export const HOST = "https://nv-blog-api.onrender.com";
-export const HOST = "http://localhost:4000";
+export const HOST = "https://nv-blog-api.onrender.com";
+// export const HOST = "http://localhost:4000";
